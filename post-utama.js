dce14d99eca23c7e6ab1fcfe63c20167d1c654a1
dedi009 <46062371+dedi009@users.noreply.github.com>
2020-10-18 12:15:21 +0800

    Update post-utama.js

--- post-utama.js
+++ post-utama.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="forex-is-invest-of-future.html"}a_to_ve[i].href="https://zip-link.blogspot.com/2020/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="2020/07/forex-is-invest-of-future.html"}a_to_ve[i].href="https://zip-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
